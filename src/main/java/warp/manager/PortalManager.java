dc8da27d23a896f27112a1939a22f4acd3c724b8
angelless <dongkyu91622@gmail.com>
2016-10-12 00:23:44 +0900

    Update PortalManager.java

--- src/main/java/warp/manager/PortalManager.java
+++ src/main/java/warp/manager/PortalManager.java
@@ -43,16 +43,22 @@ public class PortalManager {
 	*   "target" : "spawn"
 	* }
 	*/
+	public boolean addPortal(String name,Player player,String target){
+			this.addPortal(name, player.getX(),player.getY(),player.getZ(),player.getLevel(), target);
+		}
+	 
 	public boolean addPortal(String name,double x,double y,double z,Level level,String target){
+		if(!this.manager.isWarp()){
+			return false;
+		}
 		LinkedHashMap<String, Object> data = new LinkedHashMap<String,Object>();
 		data.put("x", String.format("%.1f", x));
 		data.put("y", String.format("%.1f", y));
 		data.put("z", String.format("%.1f", z));
 		data.put("level", level.getFolderName());
 		data.put("target", target);
-	}
-        public boolean addPortal(String name,Player player,String target){
-		//TODO 포탈을 추가하는 메서드
+		
+		return true;
 	}
 	public void delPortal(String name){
 		//TODO 포탈을 삭제하는 메서드
